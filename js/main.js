204a93a10f9374f7dd6aca75dc7a9a37bb010b4a
Loris Matteri <loris.matteri93@gmail.com>
2020-11-11 18:00:39 +0100

    JS embrionale e dettagli
    provato a inserire javascript così per diletto e sistemato alcuni errori

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Secondo h2 in Covers section
+document.getElementById('title').innerHTML='Creato per Artemio Astemio';
+
+// Iconcine footer sinistra
+document.getElementById('icons').innerHTML='<i class="fas fa-cat"></i><i class="fas fa-dog"></i>';
